8c84b6563677d573dcea9539acb572db1a0a7831
yamen22-dev <120108370+yamen22-dev@users.noreply.github.com>
2023-02-15 13:37:28 +0100

    modals aangepast nu zijn ze responsive

--- jaarboek.php
+++ jaarboek.php
@@ -49,17 +49,16 @@
       </div>
    </div>
    <!-- Modals -->
-   <div id="modal-1" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden">
-      <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-         <div class="bg-white rounded-lg shadow-lg w-60 md:w-3/4 lg:w-2/3 h-60 md:h-3/4 lg:h-2/3 p-6 relative overflow-y-scroll">
+   <div id="modal-1" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden ">
+      <div class="fixed top-0 left-0 w-full h-screen flex items-center justify-center ">
+         <div class="overflow-y-scroll bg-white rounded-lg shadow-lg w-3/4 md:w-3/4 lg:w-2/3 h-5/6 md:h-4/4 lg:h-2/3 p-6 relative">
             <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
                <h3 class="text-xl mb-4">Naam van de opleiding, naam van de school</h3>
                <p class="overflow-y-auto  mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet fugiat quia repellendus debitis voluptate sunt odit consequatur reiciendis commodi! Earum magnam quis necessitatibus eius beatae nemo ipsam error ea possimus!</p>
             <button id="sluit-modal-1" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
-         </div>
+         </div>  
       </div>
    </div>
-   
    <div id="modal-2" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden ">
       <div class="fixed top-0 left-0 w-full h-screen flex items-center justify-center ">
          <div class="overflow-y-scroll bg-white rounded-lg shadow-lg w-3/4 md:w-3/4 lg:w-2/3 h-5/6 md:h-4/4 lg:h-2/3 p-6 relative">
@@ -70,76 +69,76 @@
          </div>  
       </div>
    </div>
-   <div id="modal-3" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden">
-         <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-            <div class="bg-white rounded-lg shadow-lg w-60 md:w-3/4 lg:w-2/3 h-60 md:h-3/4 lg:h-2/3 p-6 relative overflow-y-scroll">
-               <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
+   <div id="modal-3" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden ">
+      <div class="fixed top-0 left-0 w-full h-screen flex items-center justify-center ">
+         <div class="overflow-y-scroll bg-white rounded-lg shadow-lg w-3/4 md:w-3/4 lg:w-2/3 h-5/6 md:h-4/4 lg:h-2/3 p-6 relative">
+            <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
                <h3 class="text-xl mb-4">Naam van de opleiding, naam van de school</h3>
                <p class="overflow-y-auto  mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet fugiat quia repellendus debitis voluptate sunt odit consequatur reiciendis commodi! Earum magnam quis necessitatibus eius beatae nemo ipsam error ea possimus!</p>
-               <button id="sluit-modal-3" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
-            </div>
-         </div>
+            <button id="sluit-modal-3" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
+         </div>  
       </div>
-   <div id="modal-4" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden">
-         <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-            <div class="bg-white rounded-lg shadow-lg w-60 md:w-3/4 lg:w-2/3 h-60 md:h-3/4 lg:h-2/3 p-6 relative overflow-y-scroll">
-               <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
+   </div>
+   <div id="modal-4" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden ">
+      <div class="fixed top-0 left-0 w-full h-screen flex items-center justify-center ">
+         <div class="overflow-y-scroll bg-white rounded-lg shadow-lg w-3/4 md:w-3/4 lg:w-2/3 h-5/6 md:h-4/4 lg:h-2/3 p-6 relative">
+            <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
                <h3 class="text-xl mb-4">Naam van de opleiding, naam van de school</h3>
                <p class="overflow-y-auto  mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet fugiat quia repellendus debitis voluptate sunt odit consequatur reiciendis commodi! Earum magnam quis necessitatibus eius beatae nemo ipsam error ea possimus!</p>
-               <button id="sluit-modal-4" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
-            </div>
-         </div>
+            <button id="sluit-modal-4" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
+         </div>  
       </div>
-   <div id="modal-5" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden">
-         <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-            <div class="bg-white rounded-lg shadow-lg w-60 md:w-3/4 lg:w-2/3 h-60 md:h-3/4 lg:h-2/3 p-6 relative overflow-y-scroll">
-               <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
+   </div>
+   <div id="modal-5" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden ">
+      <div class="fixed top-0 left-0 w-full h-screen flex items-center justify-center ">
+         <div class="overflow-y-scroll bg-white rounded-lg shadow-lg w-3/4 md:w-3/4 lg:w-2/3 h-5/6 md:h-4/4 lg:h-2/3 p-6 relative">
+            <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
                <h3 class="text-xl mb-4">Naam van de opleiding, naam van de school</h3>
                <p class="overflow-y-auto  mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet fugiat quia repellendus debitis voluptate sunt odit consequatur reiciendis commodi! Earum magnam quis necessitatibus eius beatae nemo ipsam error ea possimus!</p>
-               <button id="sluit-modal-5" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
-            </div>
-         </div>
+            <button id="sluit-modal-5" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
+         </div>  
       </div>
-   <div id="modal-6" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden">
-         <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-            <div class="bg-white rounded-lg shadow-lg w-60 md:w-3/4 lg:w-2/3 h-60 md:h-3/4 lg:h-2/3 p-6 relative overflow-y-scroll">
-               <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
+   </div>
+   <div id="modal-6" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden ">
+      <div class="fixed top-0 left-0 w-full h-screen flex items-center justify-center ">
+         <div class="overflow-y-scroll bg-white rounded-lg shadow-lg w-3/4 md:w-3/4 lg:w-2/3 h-5/6 md:h-4/4 lg:h-2/3 p-6 relative">
+            <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
                <h3 class="text-xl mb-4">Naam van de opleiding, naam van de school</h3>
                <p class="overflow-y-auto  mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet fugiat quia repellendus debitis voluptate sunt odit consequatur reiciendis commodi! Earum magnam quis necessitatibus eius beatae nemo ipsam error ea possimus!</p>
-               <button id="sluit-modal-6" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
-            </div>
-         </div>
+            <button id="sluit-modal-6" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
+         </div>  
       </div>
-   <div id="modal-7" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden">
-         <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-            <div class="bg-white rounded-lg shadow-lg w-60 md:w-3/4 lg:w-2/3 h-60 md:h-3/4 lg:h-2/3 p-6 relative overflow-y-scroll">
-               <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
+   </div>
+   <div id="modal-7" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden ">
+      <div class="fixed top-0 left-0 w-full h-screen flex items-center justify-center ">
+         <div class="overflow-y-scroll bg-white rounded-lg shadow-lg w-3/4 md:w-3/4 lg:w-2/3 h-5/6 md:h-4/4 lg:h-2/3 p-6 relative">
+            <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
                <h3 class="text-xl mb-4">Naam van de opleiding, naam van de school</h3>
                <p class="overflow-y-auto  mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet fugiat quia repellendus debitis voluptate sunt odit consequatur reiciendis commodi! Earum magnam quis necessitatibus eius beatae nemo ipsam error ea possimus!</p>
-               <button id="sluit-modal-7" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
-            </div>
-         </div>
+            <button id="sluit-modal-7" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
+         </div>  
       </div>
-   <div id="modal-8" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden">
-         <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-            <div class="bg-white rounded-lg shadow-lg w-60 md:w-3/4 lg:w-2/3 h-60 md:h-3/4 lg:h-2/3 p-6 relative overflow-y-scroll">
-               <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
+   </div>
+   <div id="modal-8" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden ">
+      <div class="fixed top-0 left-0 w-full h-screen flex items-center justify-center ">
+         <div class="overflow-y-scroll bg-white rounded-lg shadow-lg w-3/4 md:w-3/4 lg:w-2/3 h-5/6 md:h-4/4 lg:h-2/3 p-6 relative">
+            <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
                <h3 class="text-xl mb-4">Naam van de opleiding, naam van de school</h3>
                <p class="overflow-y-auto  mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet fugiat quia repellendus debitis voluptate sunt odit consequatur reiciendis commodi! Earum magnam quis necessitatibus eius beatae nemo ipsam error ea possimus!</p>
-               <button id="sluit-modal-8" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
-            </div>
-         </div>
+            <button id="sluit-modal-8" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
+         </div>  
       </div>
-   <div id="modal-9" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden">
-         <div class="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-            <div class="bg-white rounded-lg shadow-lg w-60 md:w-3/4 lg:w-2/3 h-60 md:h-3/4 lg:h-2/3 p-6 relative overflow-y-scroll">
-               <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
+   </div>
+   <div id="modal-9" class="fixed inset-0 bg-gray-500 bg-opacity-75 z-50 hidden ">
+      <div class="fixed top-0 left-0 w-full h-screen flex items-center justify-center ">
+         <div class="overflow-y-scroll bg-white rounded-lg shadow-lg w-3/4 md:w-3/4 lg:w-2/3 h-5/6 md:h-4/4 lg:h-2/3 p-6 relative">
+            <h2 class="text-5xl font-semibold mb-10">Titel van het project</h2>
                <h3 class="text-xl mb-4">Naam van de opleiding, naam van de school</h3>
                <p class="overflow-y-auto  mb-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet fugiat quia repellendus debitis voluptate sunt odit consequatur reiciendis commodi! Earum magnam quis necessitatibus eius beatae nemo ipsam error ea possimus!</p>
-               <button id="sluit-modal-9" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
-            </div>
-         </div>
+            <button id="sluit-modal-9" class="absolute right-0 top-0 mt-2 mr-5 my-10">x</button>
+         </div>  
       </div>
+   </div>
    <!-- Section for displaying teams -->
    <div id="section-overzicht-jaarboek-page teams" class="container mx-auto px-4">
     <!-- Section header -->
@@ -208,27 +207,27 @@
 
    <!-- in cijfers section -->
    <div class="py-8 px-4 sm:px-6 lg:px-8">
-  <h2 class="text-3xl font-bold text-center my-4">Onze werk in cijfers</h2>
-  <div id="container-section-cijfers" class="flex flex-col md:flex-row md:justify-between">
-    <!-- in vijf jaar tijd -->
-    <div class="flex flex-col mb-10 md:ml-3 text-center">
-      <span class="mdi mdi-calendar-text-outline text-7xl md:text-9xl text-center"></span>
-      <h3 class="text-3xl">In vijf jaar tijd</h3>
-      <p>We hebben 100 levens gered of makkelijker gemaakt!!</p>
-    </div>
-    <!-- drie generaties -->
-    <div class="flex flex-col mb-10 md:ml-3 text-center">
-      <span class="mdi mdi-human-male-board-poll text-7xl md:text-9xl text-center"></span>
-      <h3 class="text-3xl">Met behulp Deelnemers</h3>
-      <p>Met behulp van 167 duizend noorderlingen</p>
-    </div>
-    <div class="flex flex-col mb-10 md:ml-3 text-center">
-      <span class="mdi mdi-account-group text-7xl md:text-9xl text-center"></span>
-      <h3 class="text-3xl">Drie generaties</h3>
-      <p>We hebben 100 levens gered of makkelijker gemaakt!!</p>
-    </div>
-  </div>
-</div>
+      <h2 class="text-3xl font-bold text-center my-4">Onze werk in cijfers</h2>
+      <div id="container-section-cijfers" class="flex flex-col md:flex-row md:justify-between">
+         <!-- in vijf jaar tijd -->
+         <div class="flex flex-col mb-10 md:ml-3 text-center">
+            <span class="mdi mdi-calendar-text-outline text-7xl md:text-9xl text-center"></span>
+            <h3 class="text-3xl">In vijf jaar tijd</h3>
+            <p>We hebben 100 levens gered of makkelijker gemaakt!!</p>
+         </div>
+         <!-- drie generaties -->
+         <div class="flex flex-col mb-10 md:ml-3 text-center">
+            <span class="mdi mdi-human-male-board-poll text-7xl md:text-9xl text-center"></span>
+            <h3 class="text-3xl">Met behulp Deelnemers</h3>
+            <p>Met behulp van 167 duizend noorderlingen</p>
+         </div>
+         <div class="flex flex-col mb-10 md:ml-3 text-center">
+            <span class="mdi mdi-account-group text-7xl md:text-9xl text-center"></span>
+            <h3 class="text-3xl">Drie generaties</h3>
+            <p>We hebben 100 levens gered of makkelijker gemaakt!!</p>
+         </div>
+      </div>
+   </div>
 
 
 
