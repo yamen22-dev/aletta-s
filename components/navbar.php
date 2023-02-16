03170619013a77c546b9d1cdce97b49395b24581
Tim Nieborg <tppnieborg@st.noorderpoort.nl>
2023-02-16 15:44:59 +0100

    Navbar update

--- components/navbar.php
+++ components/navbar.php
@@ -1,52 +1,71 @@
-<header class="inline">
-  <!-- Navigation bar -->
-  <nav class="navbar-fixed-top flex md:justify-center justify-between px-10 md:pr-0 items-center py-3 md:py-4 lg:py-6 sticky top-0">
-    <div class="flex items-center mb-4 md:ml-8 lg:ml-16">
-      <!-- Logo -->
-      <a href="index.php">
-        <img class="h-16 lg:h-20" src="img/aletta-blue-logo.png" alt="Logo">
-      </a>
-    </div>
+<body
 
-    <div class="flex items-center justify-between">
-      <!-- Hamburger button for small screens -->
-      <button id="burger-menu" class="block md:hidden">
-        <i class="fa duotone fa-bars"></i>
-      </button>
-
-      <!-- Main menu -->
-      <ul id="main-menu" class="hidden md:flex md:flex-row md:items-center md:space-x-8 lg:space-x-16 md:mr-8">
-      <li>
-          <a href="jaarboek.php" class="px-2 py-1.5 md:px-3 md:py-2 rounded">
-            Jaarboek
+  >
+  <header class="inline">
+     <nav
+        class="
+          sticky
+          top-0
+          z-50
+          flex flex-wrap
+          items-center
+          justify-around
+          w-full
+          py-4
+          md:py-0
+          px-4
+          text-lg text-gray-700
+          bg-white
+          drop-shadow-md
+        ">
+       <div>
+          <a href="index.php">
+            <img class="w-16 m-3" src="img/aletta-blue-logo.png" alt="">
           </a>
-        </li>  
-        <li>
-            <a href="signup.php" class="px-2 py-1.5 md:px-3 md:py-2 rounded">
-                Aanmelden
-            </a>
-        </li>
-        
-      </ul>
-    </div>
-  </nav>
+        </div>
+       
+         <svg
+            xmlns="http://www.w3.org/2000/svg"
+            id="menu-button"
+            class="h-6 w-6 cursor-pointer md:hidden block"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+       
+       <div class="hidden w-full md:flex md:items-center text-center md:w-auto" id="menu">
+          <ul
+            class="
+              pt-4
+              text-base text-gray-700
+              md:flex
+              md:justify-between 
+              md:pt-0">
+              <li>
+              <a class="md:hidden lg:hidden py-2 block hover:text-primarycolor" href="index.php"
+                >Home</a>
+            </li>
+            <li>
+              <a class="md:p-4 py-2 block hover:text-primarycolor" href="jaarboek.php"
+                >Jaarboek</a>
+            </li>
+            <li>
+              <a class="md:p-4 py-2 block hover:text-primarycolor" href="signup.php"
+                >Aanmelden</a>
+            </li>
+          </ul>
+      </div>
+    </nav>
+  </header>
+</body>
 
-  <!-- Dropdown menu for small screens -->
-  <div id="dropdown-menu" class="hidden md:hidden bg-gray-100">
-    <ul class="flex flex-col items-center py-2">
-      <li class="my-2">
-        <a href="signup.php" class="block px-2 py-1.5 md:px-3 md:py-2 rounded">
-          Aanmelden
-        </a>
-      </li>
-      <li class="my-2">
-        <a href="jaarboek.php" class="block px-2 py-1.5 md:px-3 md:py-2 rounded">
-          Jaarboek
-        </a>
-      </li>
-    </ul>
-  </div>
-</header>
 
 <!-- jQuery library and custom script -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
-const form = document.querySelector('form');
-// when scrollt will the navbar change the color to white
-$(function () {
-    $(document).scroll(function () {
-        let $nav = $(".navbar-fixed-top");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-    });
-});
+// const form = document.querySelector('form');
+// // when scrollt will the navbar change the color to white
+// $(function () {
+//     $(document).scroll(function () {
+//         let $nav = $(".navbar-fixed-top");
+//         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//     });
+// });
 
-// burger menu functions
-const burgerMenu = document.getElementById('burger-menu');
-const dropdownMenu = document.getElementById('dropdown-menu');
+// // burger menu functions
+// const burgerMenu = document.getElementById('burger-menu');
+// const dropdownMenu = document.getElementById('dropdown-menu');
 
-burgerMenu.addEventListener('click', () => {
-    dropdownMenu.classList.toggle('hidden');
+// burgerMenu.addEventListener('click', () => {
+//     dropdownMenu.classList.toggle('hidden');
+// });
+
+const button2 = document.querySelector('#menu-button');
+const menu = document.querySelector('#menu');
+
+
+button2.addEventListener('click', () => {
+    menu.classList.toggle('hidden');
 });
