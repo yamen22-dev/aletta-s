1a9633c3223b996618eab41a9ad0eaccd8b899d4
PascalStudent <pswwiersma@st.noorderpoort.nl>
2023-02-16 11:57:58 +0100

    few fixes

--- signup.php
+++ signup.php
@@ -25,7 +25,7 @@
     require("components/header.php");
     ?>
     <main>
-        <div class="flex justify-center items-center">
+        <div class="flex justify-center items-center mb-32">
             <div class="container mx-auto my-4 px-4 lg:px-20">
                 <form method="POST">
                     <div class="bg-primarycolor w-full p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
@@ -92,22 +92,14 @@
 
                 <div class="w-full lg:-mt-96 lg:w-2/6 px-8 py-12 ml-auto bg-secondarycolor rounded-2xl">
                     <div class="flex flex-col text-white">
-                        <h1 class="font-bold uppercase text-4xl my-4">Vul hier in!</h1>
+                        <h1 class="font-bold uppercase text-4xl my-4 text-white">Vul hier in!</h1>
                         <p class="text-black">Vul in dit formulier uw team em teamleden in zodat wij dit kunnen gebruikten!</p>
                         <div class="flex my-4 w-2/3 lg:w-1/2">
                             <div class="flex flex-col">
-                                <h2 class="text-2xl mb-2 font-bold">Contact:</h2>
+                                <h2 class="text-2xl mb-2 font-bold text-white">Contact:</h2>
                                 <p class="text-black">Tel: <a href="tel:0612345687">0612345687</a></p>
                                 <p class="text-black">Email: <a href="mailto:info@aletta.com">info@aletta.com</a></p>
                             </div>
-                        </div>
-                        <div class="flex my-4 w-2/3 lg:w-1/2">
-                            <a href="https://www.facebook.com/ENLIGHTENEERING/" target="_blank" rel="noreferrer" class="rounded-full bg-white h-8 w-8 inline-block mx-1 text-center pt-1">
-                                <i class="fab fa-facebook-f text-blue-900" />
-                            </a>
-                            <a href="https://www.linkedin.com/company/enlighteneering-inc-" target="_blank" rel="noreferrer" class="rounded-full bg-white h-8 w-8 inline-block mx-1 text-center pt-1">
-                                <i class="fab fa-linkedin-in text-blue-900" />
-                            </a>
                         </div>
                     </div>
                 </div>
